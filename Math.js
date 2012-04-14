6c986d85947e0e8c11fb0a403255675bca6f6d96
D. Rimron <darran@xalior.com>
2012-04-14 23:05:58 +0100

    First Version

---
+++ Math.js
@@ -0,0 +1,12 @@
+/*
+ * This is not a Math Library,
+ *  (this is just a tribute)
+ *
+ * This is just a set of helpers, to make code easier to maintain.
+ *
+ * D. Rimron <darran@xalior.com>
+ */
+
+function randomint(floor, ceil) {
+    return Math.floor((Math.random()*ceil)+floor);
+}
