91326f949456f665a2041867b2741835c40d3934
D. Rimron <darran@xalior.com>
2012-04-15 04:42:31 +0100

    Remove debug data, idiotchild!
    -Dx

--- CSS.js
+++ CSS.js
@@ -1,8 +1,11 @@
 djsex.css = {
+    /*
+     * http://stackoverflow.com/questions/524696/how-to-create-a-style-tag-with-javascript
+     */
     create: function(stylesheet) {
         var head = document.getElementsByTagName('head')[0],
             style = document.createElement('style'),
-            rules = document.createTextNode('h1 { background: red; }');
+            rules = document.createTextNode(stylesheet);
 
         style.type = 'text/css';
         if(style.styleSheet)
