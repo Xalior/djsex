23898b8e86c557baf080a31d7986b0f7dab9ac20
D. Rimron <darran@xalior.com>
2012-06-12 14:03:02 +0100

    Adding in a more intelligent "onload"....
    -Dx

--- Core.js
+++ Core.js
@@ -91,6 +91,47 @@ var djsex = {
 	// The base Class implementation (does nothing)
     Class: function(){
     },
-};
 
+    onDOMReady: function(fn, ctx){
+        alert("domedone");
+        var ready, timer;
+        var onChange = function(e){
+            if(e && e.type == "DOMContentLoaded"){
+                fireDOMReady();
+            }else if(e && e.type == "load"){
+                fireDOMReady();
+            }else if(document.readyState){
+                if((/loaded|complete/).test(document.readyState)){
+                    fireDOMReady();
+                }else if(!!document.documentElement.doScroll){
+                    try{
+                        ready || document.documentElement.doScroll('left');
+                    }catch(e){
+                        return;
+                    }
+                    fireDOMReady();
+                }
+            }
+        };
+ 
+        var fireDOMReady = function(){
+            if(!ready){
+                ready = true;
+                fn.call(ctx || window);
+                if(document.removeEventListener)
+                    document.removeEventListener("DOMContentLoaded", onChange, false);
+                document.onreadystatechange = null;
+                window.onload = null;
+                clearInterval(timer);
+                timer = null;
+            }
+        };
+ 
+        if(document.addEventListener)
+            document.addEventListener("DOMContentLoaded", onChange, false);
+        document.onreadystatechange = onChange;
+        timer = setInterval(onChange, 5);
+        window.onload = onChange;
+    },
+};
 djsex.init();
