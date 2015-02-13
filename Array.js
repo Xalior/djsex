4c11e88b2e1ecd3dff6d865ad5b7afb60a75c92d
D. Rimron-Soutter <darran@xalior.com>
2015-02-13 16:18:27 +0000

    Adding Array helper - used for some gamestuff :)
    -Dx

---
+++ Array.js
@@ -0,0 +1,14 @@
+/*
+ * This is not a Array Library,
+ *  (this is just a tribute)
+ *
+ * This is just a set of helpers, to make code easier to maintain.
+ *
+ * D. Rimron <darran@xalior.com>
+ */
+
+djsex.array = {
+    randomIndex: function(arr) {
+        return Math.floor(Math.random()*arr.length);
+    }
+};
